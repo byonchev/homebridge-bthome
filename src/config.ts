94d684947b08f9d85fb42d09410ebad0de5af7fb
Boris Yonchev <boris@yonchev.me>
2025-06-30 01:47:22 +0300

    Adopt new config in platform

---
+++ src/config.ts
@@ -0,0 +1,40 @@
+import type { PlatformConfig } from 'homebridge';
+
+export type ServiceType =
+  | 'temperature'
+  | 'humidity'
+  | 'battery'
+  | 'illuminance'
+  | 'button'
+  | 'motion'
+  | 'contact';
+
+export interface ServiceOptions {
+  lowBatteryThreshold?: number;
+}
+
+export interface ServiceConfig {
+  type: ServiceType;
+  options?: ServiceOptions;
+}
+
+export interface ServicesConfig {
+  autoDiscovery?: boolean;
+  enabled?: ServiceConfig[];
+}
+
+export interface DeviceConfig {
+  name?: string;
+  mac: string;
+  encryptionKey?: string;
+  services?: ServicesConfig;
+}
+
+export interface BluetoothConfig {
+  powerOnTimeout?: number;
+}
+
+export interface BTHomePlatformConfig extends PlatformConfig {
+  devices?: DeviceConfig[];
+  bluetooth?: BluetoothConfig;
+}
