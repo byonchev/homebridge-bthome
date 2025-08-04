e2bfced64e22c9c169cd4df681a50d6f94c24780
Boris Yonchev <boris@yonchev.me>
2025-08-04 23:40:18 +0300

    Complete support for battery sensor data

--- src/services/battery.ts
+++ src/services/battery.ts
@@ -1,27 +1,50 @@
+import { CharacteristicValue } from 'homebridge';
 import { BTHomeSensorData } from '../bthome/types.js';
 import { ServiceHandler } from './base.js';
 
 export class BatteryHandler extends ServiceHandler {
-  private readonly DEFAULT_LOW_BATTERY_THRESHOLD = 10;
+  public static matches(sensorData: BTHomeSensorData): boolean {
+    return (
+      sensorData.batteryLevel !== undefined ||
+      sensorData.batteryLow !== undefined ||
+      sensorData.batteryCharging !== undefined
+    );
+  }
 
   public updateValues(sensorData: BTHomeSensorData) {
-    if (sensorData.battery === undefined) {
+    const { batteryLow, batteryLevel, batteryCharging } = sensorData;
+
+    const lowBatteryStatus = this.mapLowBatteryStatus(batteryLow, batteryLevel);
+    const chargingState = this.mapChargingState(batteryCharging);
+
+    this.service.getCharacteristic(this.Characteristic.StatusLowBattery).updateValue(lowBatteryStatus);
+
+    if (batteryLevel !== undefined) {
+      this.service.getCharacteristic(this.Characteristic.BatteryLevel).updateValue(batteryLevel);
+    }
+
+    if (chargingState !== undefined) {
+      this.service.getCharacteristic(this.Characteristic.ChargingState).updateValue(chargingState);
+    }
+  }
+
+  private mapLowBatteryStatus(reportedLow?: boolean, reportedLevel?: number): CharacteristicValue {
+    const threshold = this.options?.lowBatteryThreshold;
+
+    if (reportedLevel !== undefined && threshold !== undefined && reportedLevel < threshold) {
+      return this.Characteristic.StatusLowBattery.BATTERY_LEVEL_LOW;
+    } else if (reportedLow) {
+      return this.Characteristic.StatusLowBattery.BATTERY_LEVEL_LOW;
+    } else {
+      return this.Characteristic.StatusLowBattery.BATTERY_LEVEL_NORMAL;
+    }
+  }
+
+  private mapChargingState(charging?: boolean): CharacteristicValue | undefined {
+    if (charging === undefined) {
       return;
     }
 
-    this.service.getCharacteristic(this.Characteristic.BatteryLevel).updateValue(sensorData.battery);
-
-    const lowBatteryThreshold = this.options?.lowBatteryThreshold ?? this.DEFAULT_LOW_BATTERY_THRESHOLD;
-
-    const status =
-      sensorData.battery < lowBatteryThreshold
-        ? this.Characteristic.StatusLowBattery.BATTERY_LEVEL_LOW
-        : this.Characteristic.StatusLowBattery.BATTERY_LEVEL_NORMAL;
-
-    this.service.getCharacteristic(this.Characteristic.StatusLowBattery).updateValue(status);
-  }
-
-  public static matches(sensorData: BTHomeSensorData): boolean {
-    return sensorData.battery !== undefined;
+    return charging ? this.Characteristic.ChargingState.CHARGING : this.Characteristic.ChargingState.NOT_CHARGING;
   }
 }
